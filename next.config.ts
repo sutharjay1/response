f53faaa3c36c4fcb39dcc2a2802a92a88321f46c
sutharjay1 <sutharjay3635@gmail.com>
2024-12-13 21:17:50 +0530

    Bugs fix and improvement

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 const nextConfig: NextConfig = {
   images: {
@@ -28,9 +27,5 @@ const nextConfig: NextConfig = {
     },
   },
 };
-
-if (process.env.NODE_ENV === "development") {
-  await setupDevPlatform();
-}
 
 export default nextConfig;
